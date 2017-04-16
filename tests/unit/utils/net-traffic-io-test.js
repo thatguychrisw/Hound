d09c65841267a9e29e9920a31b33db453031056c
Christopher Willard <thatguychrisw@gmail.com>
2017-04-16 19:55:16 -0400

    Update to structure

--- tests/unit/utils/net-traffic-io-test.js
+++ tests/unit/utils/net-traffic-io-test.js
@@ -4,5 +4,6 @@ module('Unit | Utility | get network traffic input/output');
 
 test('test loaded', function (assert) {
     // Not all that useful, just for example's sake
+    // There will also be {source}-net-traffic-io.js
     assert.ok(true);
 });
