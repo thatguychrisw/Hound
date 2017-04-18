8e04b9539d08a1c978d9d4970efc089d135f9b5c
Chava Sobreyra <chava.sobreyra@gmail.com>
2017-04-17 22:15:28 -0400

    scaffold util, add pseudo code descs for service api

--- tests/unit/services/speed-test-test.js
+++ tests/unit/services/speed-test-test.js
@@ -5,6 +5,20 @@ moduleFor('service:speed-test', 'Unit | Service | speed test', {
   // needs: ['service:foo']
 });
 
+
+/**
+ * Some pseudo code description of the service api
+ *
+ * test(provider) - a single one-off test, provider params is optional, returns results
+ *
+ * start(provider) - starts running tests at continuous interval,  provider param is optional, puts results in store as they come in
+ *
+ * stop() - stop running tests
+ *
+ * status - current testing status
+ *
+ */
+
 // Replace this with your real tests.
 test('it exists', function(assert) {
   let service = this.subject();
